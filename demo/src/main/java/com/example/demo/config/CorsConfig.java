e3c9a27d910be6eb61753d2092971307f096bb11
HC <hemachand2508@gmail.com>
2025-09-22 23:40:09 +0530

    Nodified cors config

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                             "http://localhost:5173",  // local dev
-                            "http://localhost:8080"   // deployed frontend
+                            "http://localhost:8080",
+                            "http://localhost:2030"// deployed frontend
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
